64e6f620ed4d00f8865ba2895ef0ad710cc5efdd
Freja Fischer <frej657e@stud.kea.dk>
2025-04-22 09:18:54 +0200

    added db config credentials

--- App/Config.php
+++ App/Config.php
@@ -5,4 +5,10 @@ namespace App;
 abstract class Config 
 {
     const SHOW_ERRORS = true; // true for development, else false
+
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'library_api';
+    const DB_USER = 'root';
+    const DB_PORT = '8889';
+    const DB_PASSWORD = 'root';
 }
