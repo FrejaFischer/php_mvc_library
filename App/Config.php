7742e9b0846b26f1627d83acf7eceafcbf23792e
Freja Fischer <frej657e@stud.kea.dk>
2025-04-22 09:47:33 +0200

    added base_url

--- App/Config.php
+++ App/Config.php
@@ -4,6 +4,8 @@ namespace App;
 
 abstract class Config 
 {
+    const BASE_URL = '/php_mvc_library/public/';
+
     const SHOW_ERRORS = true; // true for development, else false
 
     const DB_HOST = 'localhost';
